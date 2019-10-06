2ac45415f6b404105554c12fe6852485f5c768c2
eldenhowes <williameldenhowes@gmail.com>
2019-10-06 13:32:05 +0100

    Basic styling on Home page sorted

--- app.rb
+++ app.rb
@@ -6,5 +6,21 @@ class Portfolio < Sinatra::Base
     File.read('public/index.html')
   end
 
+  get '/cv' do
+    File.read('public/cv.html')
+  end
+
+  get '/projects' do
+    File.read('public/projects.html')
+  end
+
+  get '/aboutme' do
+    File.read('public/aboutme.html')
+  end
+
+  get '/contact' do
+    File.read('public/contact.html')
+  end
+
   run! if app_file == $0
 end
